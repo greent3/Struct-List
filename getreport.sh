b6501101d0a5ab12fc74128ba7f271837bc3cbbe
Travis Green <greent3@hawaii.edu>
2019-11-03 23:00:28 -1000

    fixed .sh file

--- getreport.sh
+++ getreport.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git --no-pager log --graph --date-order --all --pretty=format: '%h %Cred%d %Cgreen%ad %Cblue%cn %Creset%s' --date=short
-echo "Hello"
+git --no-pager log --graph --date-order --all --pretty=format:'%h %Cred%d %Cgreen%ad %Cblue%cn %Creset%s' --date=short
+echo
 git status
 ls -la
 rm *.o
